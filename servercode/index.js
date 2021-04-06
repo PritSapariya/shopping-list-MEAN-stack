569633b7af5f2a09cc19deaa743f396e3b91e3d3
PritSapariya <pritsapariya17@gmail.com>
2021-04-06 19:05:40 +0530

    Connecting to MongoDb database

--- servercode/index.js
+++ servercode/index.js
@@ -4,3 +4,19 @@ var bodyparser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/shoppinglist' );
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('Server Started at port :' + PORT);
+});
