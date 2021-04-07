f241e56c74414cf4c1efcf343366e165d72ed49b
PritSapariya <pritsapariya17@gmail.com>
2021-04-07 15:42:31 +0530

    Roter created

--- servercode/route/routes.js
+++ servercode/route/routes.js
@@ -1,20 +1,73 @@
 var express = require('express');
 var router = express.Router();
+var Item = require('../model/shoppingItem.js');
 
-router.get('/test', (req, res, next) => {
-    res.send("Testing Route")
+router.get('/items', (req, res, next) => {
+    Item.find(function(err, items){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(items);
+        }
+    })
 });
 
-router.post('/test', (req, res, next) => {
-    // Do later
+router.post('/item', (req, res, next) => {
+    
+    let newShoppingItem = new Item({
+        itemName: req.body.itemName,
+        itemQuantity: req.body.itemQuantity,
+        itemBought: req.body.itemBought
+    });
+
+    newShoppingItem.save((err, item) => {
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json({msg: 'Item has been added to DB'});
+        }
+    });
+
 });
 
-router.put('/test', (req, res, next) => {
-    // Do later
+router.put('/item/:id', (req, res) => {
+
+    item.findOneAndUpdate({_id: res.params.id}, {
+        $set: {
+            itemName: req.body.itemName,
+            itemQuantity: req.body.itemQuantity,
+            itemBought: req.body.itemBought
+        }
+    },
+    function(err, results){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(results);
+        }
+    });
 });
 
-router.delete('/test', (req, res, next) => {
-    // Do later
+router.delete('/item/:id', (req, res) => {
+    
+    item.remove({_id: res.params.id}, {
+        $set: {
+            itemName: req.body.itemName,
+            itemQuantity: req.body.itemQuantity,
+            itemBought: req.body.itemBought
+        }
+    },
+    function(err, results){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(results);
+        }
+    });
 });
 
 
