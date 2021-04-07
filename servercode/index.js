fed5d576227b8546ea33dd3d1c23bd33da1dff9b
PritSapariya <pritsapariya17@gmail.com>
2021-04-07 14:39:19 +0530

    Setting up Route and Middlewares

--- servercode/index.js
+++ servercode/index.js
@@ -5,6 +5,8 @@ var cors = require('cors');
 
 var app = express();
 
+const route = require('./route/route.js');
+
 mongoose.connect('mongodb://localhost:27017/shoppinglist' );
 
 mongoose.connection.on('connected', () => {
@@ -15,7 +17,18 @@ mongoose.connection.on('error', (err) => {
     console.log(err);
 });
 
+
 const PORT = 3000;
+
+app.use(cors());                            // This will help to communicate with two different PORT
+
+app.use(bodyparser.json());
+
+app.use('/api', route);
+
+app.get('/', (req, res) => {
+    res.send("Testing Route ...");
+});
 
 app.listen(PORT, () => {
     console.log('Server Started at port :' + PORT);
