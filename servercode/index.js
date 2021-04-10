5bc103225f2d007c9647ba703492127d79bfeee7
PritSapariya <pritsapariya17@gmail.com>
2021-04-10 22:32:47 +0530

    Connection Updated

--- servercode/index.js
+++ servercode/index.js
@@ -8,21 +8,17 @@ var app = express();
 
 const route = require('./route/routes.js');
 
-mongoose.connect('mongodb://localhost:27017/shoppinglist' );
-
-mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.log(err);
-});
+mongoose.connect('mongodb://localhost:27017/soppinglist', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+})
+.then(() => console.log("MongoDB server is connected"))
+.catch((err) => console.log(err));
 
 
 const PORT = process.env.PORT;
 
-app.use(cors());                            // This will help to communicate with two different PORT
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', route);
